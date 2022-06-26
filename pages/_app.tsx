0195eb82dd992bc00b2df963b4d314e93ebc6213
haesoo <wjdgotn77@gmail.com>
2022-06-26 22:10:17 +0900

    add: index.css 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,8 @@
-import type { AppProps } from "next/app";
-import { ChakraProvider } from "@chakra-ui/react";
-// 글로벌 css 여기
+import type { AppProps } from 'next/app';
+import 'styles/index.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-      <Component {...pageProps} />
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
