f840370169a4ed20fb2b2b3674b1c0f9a11986c4
haesoo Jeong <wjdgotn77@gmail.com>
2022-07-11 23:55:49 +0900

    add: threejs.tsx

---
+++ pages/threejs.tsx
@@ -0,0 +1,12 @@
+import { styled } from "stitches.config";
+
+const Div = styled("div", {
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  p: "20px",
+});
+
+export default function ThreeJSPage() {
+  return <Div>To be continue ...</Div>;
+}
