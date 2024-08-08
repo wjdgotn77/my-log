51a572ac647f010272422c597d88e41b4c302729
Haesoo Jeong <wjdgotn77@gmail.com>
2024-08-08 16:22:15 +0900

    feat : update list styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,13 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    listStyleType: {
+      none: "none",
+      disc: "disc",
+      decimal: "decimal",
+      square: "square",
+      roman: "upper-roman",
+    },
   },
   plugins: [],
   mode: "jit",
