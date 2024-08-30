9f7a9c0dd950bbd7ba2213464a2ed788ec24281b
Haesoo Jeong <wjdgotn77@gmail.com>
2024-08-30 18:29:49 +0900

    Fix : rss feed site url

--- src/app/rss.xml/route.ts
+++ src/app/rss.xml/route.ts
@@ -11,7 +11,7 @@ export async function GET() {
       "안녕하세요, 식물을 키우고 요리하는 것을 좋아하며 늘 살아보고 싶었던 서촌에서 지내고 있는 프론트엔드 개발자 정해수입니다. 이곳에서는 개인적인 생각을 정리하고, 프로젝트를 통해 얻은 것을 기록하면서 지금까지의 여정을 돌아보고 앞으로 나아가는 과정을 적어보려 합니다. 또 이를 통해 인생을 제대로 살아보기 위한 고민들을 솔직하게 정리합니다.",
     generator: "RSS for Node and Next.js",
     feed_url: `${domain}feed.xml`,
-    site_url: `${domain}`,
+    site_url: `${domain}blog`,
     copyright: `Copyright ${new Date().getFullYear().toString()}`,
     language: "ko-KR",
     managingEditor: "wjdgotn77@gmail.com (Haesoo Cheong)",
